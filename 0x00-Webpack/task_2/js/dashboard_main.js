19f57b1085d2bd15067d8c3eaf669e8655eaeba0
faith-Iminza <52449998+faith-Iminza@users.noreply.github.com>
2023-08-22 15:56:44 +0300

    update

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,22 +1,18 @@
+import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(function () {
-  // Create and append elements to the body
-  $('<p>Holberton Dashboard</p>').appendTo('body');
-  $('<p>Dashboard data for the students</p>').appendTo('body');
-  $('<button>Click here to get started</button>').appendTo('body');
-  $('<p id="count"></p>').appendTo('body');
-  $('<p>Copyright - Holberton School</p>').appendTo('body');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-  // Function to update the counter
-  let count = 0;
-  const updateCounter = _.debounce(function () {
-    count++;
-    $('#count').text(count + ' clicks on the button');
-  }, 500);
+let conunt = 0;
 
-  // Bind the debounce function to the click event of the button
-  $('button').on('click', updateCounter);
-});
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,62 +1,38 @@
-// webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
   },
-  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        test: /\.(gif|png|jp?g|svg)$/i,
         use: [
-          'file-loader',
+          "file-loader",
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
-              disable: false,
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
+              bypassOnDebug: true,
+              disable: true,
             },
           },
         ],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css',
-    }),
-  ],
 };
-
