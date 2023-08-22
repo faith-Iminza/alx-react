afb4a2b0bb4d3c76ce43911559710b4801457bc1
faith-Iminza <faithjacks63@gmail.com>
2023-08-22 11:29:05 +0000

    task

---
+++ 0x00-Webpack/task-0/src/index.js
@@ -0,0 +1,10 @@
+// src/index.js
+import $ from 'jquery';
+
+$(document).ready(function () {
+  // Create and append paragraphs to the body
+  $('<p>Holberton Dashboard</p>').appendTo('body');
+  $('<p>Dashboard data for the students</p>').appendTo('body');
+  $('<p>Copyright - Holberton School</p>').appendTo('body');
+});
+
